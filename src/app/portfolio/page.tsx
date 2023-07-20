25594d066c19f4ce3c7df2092b5b3dfedabfbba1
Amrit Bastakoti <amritbastakoti618@gmail.com>
2023-07-20 22:23:44 +0300

    interior part

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const portfolio = () => {
   return (
-    <div>portfolio</div>
-  )
+    <>
+      <div>portfolio</div>
+      <h1>Hi, there this is a portfolio site </h1>
+    </>
+  );
 }
 
 export default portfolio
