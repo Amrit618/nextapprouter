24eba34342a5bc821e89bf85fda152ae8af43f42
Amrit Bastakoti <amritbastakoti618@gmail.com>
2023-07-25 16:03:52 +0300

    new id

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bloglayout = ({children}:any) => {
+  return (
+    <>
+      <div>Bloglayout{children}</div>
+    </>
+  );
+}
+
+export default Bloglayout
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>hey,its alright{children}</body>
+      <body className={inter.className}>
+        <h1>Hi there, from Main Layout</h1>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
