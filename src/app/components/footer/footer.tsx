9a551add1a3b3cbdb1c16bcb38461732f84a0e83
Amrit Bastakoti <amritbastakoti618@gmail.com>
2023-07-26 22:41:58 +0300

    register page modified

--- src/app/components/footer/footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const footer = () => {
-  return (
-    <div>Hi, I am going through footer page</div>
-  )
-}
-
-export default footer
--- src/app/dashboard/(auth)/register/page.tsx
+++ src/app/dashboard/(auth)/register/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Hi, this is register page</div>
+    <div>Hi, this is register page
+      <h1> Register page on RegisterBlog</h1>
+    </div>
   )
 }
 
