bbd79c8d32c832eb1daa16ec666b5083ba743876
Amrit Bastakoti <amritbastakoti618@gmail.com>
2023-07-30 19:50:24 +0300

    removed loading page

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const footer = () => {
   return (
-    <div>footer</div>
-  )
+    <>
+      <div>@2023 Amrit Bastakoti. All rights reserved.</div>
+      <div></div>
+    </>
+  );
 }
 
 export default footer
