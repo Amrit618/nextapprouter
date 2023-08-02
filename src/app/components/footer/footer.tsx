f1cf4720a3e62b6a2d695a2bd9ec305d58ec9c00
Amrit Bastakoti <amritbastakoti618@gmail.com>
2023-08-02 16:19:23 +0300

    navbar css

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,10 +1,11 @@
 'use client'
 import React from 'react'
+import styles from './footer.module.css';
 
 const footer = () => {
   return (
     <>
-      <div>@2023 Amrit Bastakoti. All rights reserved.</div>
+      <div className={styles.container}>@2023 Amrit Bastakoti. All rights reserved.</div>
       <div></div>
     </>
   );
