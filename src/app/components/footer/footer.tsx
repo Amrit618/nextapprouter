096090b3944fea5e64282a950c388076b46cb0e2
Amrit Bastakoti <amritbastakoti618@gmail.com>
2023-08-03 21:13:30 +0300

    image

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -5,8 +5,14 @@ import styles from './footer.module.css';
 const footer = () => {
   return (
     <>
-      <div className={styles.container}>@2023 Amrit Bastakoti. All rights reserved.</div>
-      <div></div>
+      <div className={styles.container}>
+        @2023 Amrit Bastakoti. All rights reserved.
+        </div>
+      <div>
+        <image src='1.png' fill:any= {true} 
+        width={} 
+        alt= 'amrit'
+      </div>
     </>
   );
 }
