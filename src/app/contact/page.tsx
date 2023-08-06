d6a759f8c1509d530d8ce4b2bfaf313c055195b7
Amrit Bastakoti <amritbastakoti618@gmail.com>
2023-08-06 21:46:11 +0300

    new on page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import styles from './page.module.css'
+import React from 'react';
+import styles from './page.module.css';
 
 const page = () => {
-  return (
-    <div className={styles.container}>Contact page</div>
-  )
-}
+  return <div className={styles.container}>Contact page</div>;
+};
 
-export default page
+export default page;
--- src/app/dashboard/(auth)/login/page.tsx
+++ src/app/dashboard/(auth)/login/page.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 import styles from './page.module.css';
 
 const Loginpage = () => {
-  return (<>
-    <div className={styles.container}>Loginpage</div>
-    
+  return (
+    <>
+      <div className={styles.container}>Loginpage</div>
     </>
-  )
-}
+  );
+};
 
-export default Loginpage
+export default Loginpage;
